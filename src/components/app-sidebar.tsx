4120f565d3136b31adce458c10f79d33cea96815
cloverzero@gmail.com <cloverzero@gmail.com>
2025-10-23 00:34:30 +0800

    basic ui layout

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import {
+  IconCamera,
+  IconChartBar,
+  IconDashboard,
+  IconFileAi,
+  IconFileDescription,
+  IconFolder,
+  IconInnerShadowTop,
+  IconListDetails,
+  IconUsers,
+} from "@tabler/icons-react";
+import * as React from "react";
+
+import { NavMain } from "@/components/nav-main";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: IconDashboard,
+    },
+    {
+      title: "Lifecycle",
+      url: "#",
+      icon: IconListDetails,
+    },
+    {
+      title: "Analytics",
+      url: "#",
+      icon: IconChartBar,
+    },
+    {
+      title: "Projects",
+      url: "#",
+      icon: IconFolder,
+    },
+    {
+      title: "Team",
+      url: "#",
+      icon: IconUsers,
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="#">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">Acme Inc.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+    </Sidebar>
+  );
+}
---
+++ src/pages/homepage.tsx
@@ -0,0 +1,12 @@
+import { SiteHeader } from "@/components/page-header";
+
+export default function Homepage() {
+  return (
+    <main>
+      <SiteHeader />
+      <div>
+        <h1>Homepage</h1>
+      </div>
+    </main>
+  );
+}
